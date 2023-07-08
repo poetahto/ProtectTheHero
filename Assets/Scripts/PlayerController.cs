21099c05d9ba5c42094df2900e421c4cca2506eb
Daniel Walls <walls.dk@gmail.com>
2023-07-08 07:56:52 -0700

    added sprinting, hopefully more smoothness

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,19 +4,27 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public float sprintSpeed;
     public float acceleration;
     public float deceleration;
     public Rigidbody2D body;
     public Vector2 inputDirection;
+    public bool isSprinting;
 
     public void InputMove(InputAction.CallbackContext context)
     {
         inputDirection = context.ReadValue<Vector2>();
     }
 
+    public void InputSprint(InputAction.CallbackContext context)
+    {
+        // 1 = button held, 0 = button not held
+        isSprinting = context.ReadValue<float>() != 0;
+    }
+
     private void Update()
     {
-        Vector2 target = inputDirection * speed;
+        Vector2 target = inputDirection * (isSprinting ? sprintSpeed : speed);
         Vector2 current = body.velocity;
 
         if (inputDirection != Vector2.zero)
