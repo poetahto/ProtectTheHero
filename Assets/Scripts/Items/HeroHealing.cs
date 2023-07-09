fd3096841d7bbe17289e1d0d2245d9caea4e7e79
Daniel Walls <walls.dk@gmail.com>
2023-07-09 09:43:09 -0700

    healing item

---
+++ Assets/Scripts/Items/HeroHealing.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class HeroHealingItem : MonoBehaviour
+    {
+        public float healing = 3.0f;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.TryGetComponent(out HeroController hero))
+            {
+                hero.heroEntity.health += healing;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
