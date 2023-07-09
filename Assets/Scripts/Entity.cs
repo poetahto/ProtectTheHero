ec1d0b167363c16b2c85f3d180a65426d41fe693
Daniel Walls <walls.dk@gmail.com>
2023-07-09 08:53:05 -0700

    death sounds

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -4,17 +4,33 @@ using UnityEngine.Events;
 public class Entity : MonoBehaviour
 {
     public AudioClip damageSound;
+    public AudioClip deathSound;
     public float health;
     public UnityEvent<float> onDamage;
+    public UnityEvent onDeath;
 
     public void Damage(float amount)
     {
+        if (health <= 0)
+        {
+            return;
+        }
+
         health -= amount;
         onDamage.Invoke(amount);
 
         if (damageSound != null)
         {
             AudioSource.PlayClipAtPoint(damageSound, transform.position);
+        }
+
+        if (health <= 0)
+        {
+            if (deathSound != null)
+            {
+                AudioSource.PlayClipAtPoint(deathSound, transform.position);
+            }
+            onDeath.Invoke();
         }
     }
 }
